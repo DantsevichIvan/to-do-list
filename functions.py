752fddc64aeaafc0227f0cc57ceb07d1271a3d4c
DantsevichIvan <iw.dantsevich@gmail.com>
2024-10-31 11:07:48 +0100

    git init

---
+++ functions.py
@@ -0,0 +1,20 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+
+    with open(filepath, "r") as file_local:
+        return file_local.readlines()
+
+
+def write_todos(value, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, "w") as file_local:
+        file_local.writelines(value)
+
+
+if __name__ == "__main__":
+    print(get_todos())

a97ff9742f92cb041ba93704c5362808bfe5633d
DantsevichIvan <iw.dantsevich@gmail.com>
2024-10-31 12:28:15 +0100

    add requirements.txt

--- gui.py
+++ gui.py
@@ -1,15 +1,10 @@
 import functions
 import FreeSimpleGUI as sg
 
-import sys
-
-print(sys.version)
-
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=(45, 10))
-
 edit_button = sg.Button("Edit")
 
 window = sg.Window("My To-Do App",
